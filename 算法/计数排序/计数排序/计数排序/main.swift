edcfdd6a11ec067325493a8cc084b0dbae29b186
yunna <yunna@xiaohuigedeMacBook-Pro.local>
2019-06-28 10:55:40 +0800

    计数排序

--- 算法/计数排序/计数排序/计数排序/main.swift
+++ 算法/计数排序/计数排序/计数排序/main.swift
@@ -4,9 +4,12 @@
 //
 //  Created by yunna on 2019/6/28.
 //  Copyright © 2019年 yunna. All rights reserved.
-//
+//[3,1,1,2,1,1]
 
 import Foundation
 
-print("Hello, World!")
+let arr = generateRandomArray(count: 10, rangL: 1, rangR: 5)
+print(arr)
+let result = CountingSort().sort(arr: arr)
+print(result)
 
